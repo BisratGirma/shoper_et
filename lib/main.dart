07d2b6ed8019d65efcff7bbcd57f551ec4f0ac18
bisrat <30285329+bisrax@users.noreply.github.com>
2022-03-11 11:34:27 +0300

    inital

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'ShopEt'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Text('Let\'s Build a shop!'),
+        ));
+  }
+}
---
+++ lib/models/product.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavourite;
+
+  Product(
+      {@required this.id,
+      @required this.title,
+      @required this.description,
+      @required this.imageUrl,
+      this.isFavourite,
+      @required this.price});
+}
